6d0b70a322eaee3b5b874f35168b7fdd06ef7686
eldovyn <farras.pramudita@gmail.com>
2025-10-06 17:13:36 +0700

    feat: add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
 </script>
 
 <template>
+    <div class="flex flex-row-reverse gap-5 me-5">
+        <Button type="submit" class="bg-blue-600 hover:bg-blue-700 cursor-pointer w-20">Sign In</Button>
+        <Button type="submit" class="bg-blue-600 hover:bg-blue-700 cursor-pointer w-20">Sign Up</Button>
+    </div>
+    <div class="flex flex-col gap-5 items-center justify-center h-screen">
+        <h1 class="text-3xl font-bold text-center">Welcome Back To Chaty</h1>
+        <Textarea class="w-[60%]" placeholder="ask me anything" />
+    </div>
 </template>
