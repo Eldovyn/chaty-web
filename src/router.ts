e879bab5972765235887b07a48824899e057a829
eldovyn <farras.pramudita@gmail.com>
2025-10-06 14:37:20 +0700

    feat: add register page

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from './views/auth/Login.vue'
+import Register from './views/auth/Register.vue'
 
 const routes = [
+    { path: '/register', component: Register },
     { path: '/login', component: Login },
 ]
 
