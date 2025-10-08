96c91e88f4652a14b2ab9988a6aaa0f20aa252c2
eldovyn <farras.pramudita@gmail.com>
2025-10-08 21:29:05 +0700

    feat: add middleware

--- src/router.ts
+++ src/router.ts
@@ -3,17 +3,31 @@ import Login from './views/auth/Login.vue'
 import Register from './views/auth/Register.vue'
 import Home from './views/Home.vue'
 import AccountActive from './views/auth/Account-Active.vue'
+import { useCookies } from 'vue3-cookies'
 
 const routes = [
     { path: '/register', component: Register, meta: { sidebar: false } },
     { path: '/login', component: Login, meta: { sidebar: false } },
-    { path: '/', component: Home, meta: { sidebar: true } },
+    { path: '/', component: Home, meta: { sidebar: true, requiresAuth: false } },
     { path: '/account-active', component: AccountActive, meta: { sidebar: false } },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+})
+
+router.beforeEach((to, from, next) => {
+    const { cookies } = useCookies()
+    const token = cookies.get('access_token')
+
+    if (to.meta.requiresAuth && !token) {
+        next('/login')
+    } else if ((to.path === '/login' || to.path === '/register') && token) {
+        next('/')
+    } else {
+        next()
+    }
 })
 
 export default router
